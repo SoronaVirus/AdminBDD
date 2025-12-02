d3f66a62e7dccaf46b3b8979abc07f0aa2f8059a
SoronaVirus <tsuki.mail14@gmail.com>
2025-12-02 14:55:50 +0100

    Fix: nom des variables

--- db_init.py
+++ db_init.py
@@ -8,34 +8,34 @@ def data_base_initialisation():
     db.monstres.drop()
     db.scores.drop()
 
-    personnages = [
-        {"nom": "Guerrier", "attack": 15, "armor": 10, "hp": 100},
-        {"nom": "Mage", "attack": 20, "armor": 5, "hp": 80},
-        {"nom": "Archer", "attack": 18, "armor": 7, "hp": 90},
-        {"nom": "Voleur", "attack": 22, "armor": 8, "hp": 85},
-        {"nom": "Paladin", "attack": 14, "armor": 12, "hp": 110},
-        {"nom": "Sorcier", "attack": 25, "armor": 3, "hp": 70},
-        {"nom": "Chevalier", "attack": 17, "armor": 15, "hp": 120},
-        {"nom": "Moine", "attack": 19, "armor": 9, "hp": 95},
-        {"nom": "Berserker", "attack": 23, "armor": 6, "hp": 105},
-        {"nom": "Chasseur", "attack": 16, "armor": 11, "hp": 100}
+    characters = [
+        {"name": "Guerrier", "attack": 15, "armor": 10, "hp": 100},
+        {"name": "Mage", "attack": 20, "armor": 5, "hp": 80},
+        {"name": "Archer", "attack": 18, "armor": 7, "hp": 90},
+        {"name": "Voleur", "attack": 22, "armor": 8, "hp": 85},
+        {"name": "Paladin", "attack": 14, "armor": 12, "hp": 110},
+        {"name": "Sorcier", "attack": 25, "armor": 3, "hp": 70},
+        {"name": "Chevalier", "attack": 17, "armor": 15, "hp": 120},
+        {"name": "Moine", "attack": 19, "armor": 9, "hp": 95},
+        {"name": "Berserker", "attack": 23, "armor": 6, "hp": 105},
+        {"name": "Chasseur", "attack": 16, "armor": 11, "hp": 100}
     ]
 
-    monstres = [
-        {"nom": "Gobelin", "attack": 10, "armor": 5, "hp": 50},
-        {"nom": "Orc", "attack": 20, "armor": 8, "hp": 120},
-        {"nom": "Dragon", "attack": 35, "armor": 20, "hp": 300},
-        {"nom": "Zombie", "attack": 12, "armor": 6, "hp": 70},
-        {"nom": "Troll", "attack": 25, "armor": 15, "hp": 200},
-        {"nom": "Spectre", "attack": 18, "armor": 10, "hp": 100},
-        {"nom": "Golem", "attack": 30, "armor": 25, "hp": 250},
-        {"nom": "Vampire", "attack": 22, "armor": 12, "hp": 150},
-        {"nom": "Loup-garou", "attack": 28, "armor": 18, "hp": 180},
-        {"nom": "Squelette", "attack": 15, "armor": 7, "hp": 90}
+    monsters = [
+        {"name": "Gobelin", "attack": 10, "armor": 5, "hp": 50},
+        {"name": "Orc", "attack": 20, "armor": 8, "hp": 120},
+        {"name": "Dragon", "attack": 35, "armor": 20, "hp": 300},
+        {"name": "Zombie", "attack": 12, "armor": 6, "hp": 70},
+        {"name": "Troll", "attack": 25, "armor": 15, "hp": 200},
+        {"name": "Spectre", "attack": 18, "armor": 10, "hp": 100},
+        {"name": "Golem", "attack": 30, "armor": 25, "hp": 250},
+        {"name": "Vampire", "attack": 22, "armor": 12, "hp": 150},
+        {"name": "Loup-garou", "attack": 28, "armor": 18, "hp": 180},
+        {"name": "Squelette", "attack": 15, "armor": 7, "hp": 90}
     ]
 
-    db.personnages.insert_many(personnages)
-    db.monstres.insert_many(monstres)
+    db.personnages.insert_many(characters)
+    db.monstres.insert_many(monsters)
     
     client.close()
 
